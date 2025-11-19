bf36e4ce44c479dcc56d501e96df4cbebdbbb5ab
tyhts0829 <tyhts0829@gmail.com>
2025-11-19 21:34:19 +0900

    📝

--- sketch/251119.py
+++ sketch/251119.py
@@ -5,7 +5,7 @@ A5 = (148, 210)
 
 def draw(t: float):
     g = G.line().scale(100, 100, 1).translate(A5[0] / 2, A5[1] / 2, 0)
-    p = E.affine().subdivide().displace().scale(scale=(1, 1, 0)).offset().offset().offset()
+    p = E.affine().subdivide().displace().scale(scale=(1, 1, 0)).offset().offset().offset().fill()
     return p(g)
 
 
@@ -18,5 +18,4 @@ if __name__ == "__main__":
         use_parameter_gui=True,
         workers=4,
         line_thickness=0.001,
-        show_hud=True,
     )
